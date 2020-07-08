cca41d0c9a4854a97b19362246ef2a6e7e80be78
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-07-07 19:35:13 -0700

    Updates

---
+++ precache-manifest.573a5b598bc96df498c323c011b029e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e27164d85d6194b2023b2a0babebe06",
+    "url": "/mz-react-portfolio/index.html"
+  },
+  {
+    "revision": "0f67c678c36ed5bc2170",
+    "url": "/mz-react-portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4ce82ff63a3e37d7ba14",
+    "url": "/mz-react-portfolio/static/css/main.a9e747f6.chunk.css"
+  },
+  {
+    "revision": "0f67c678c36ed5bc2170",
+    "url": "/mz-react-portfolio/static/js/2.4534d050.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mz-react-portfolio/static/js/2.4534d050.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce82ff63a3e37d7ba14",
+    "url": "/mz-react-portfolio/static/js/main.17023798.chunk.js"
+  },
+  {
+    "revision": "b123c936468a6bd3ef4c",
+    "url": "/mz-react-portfolio/static/js/runtime-main.4522489f.js"
+  },
+  {
+    "revision": "f75850c71112589231cfcfb53d33feec",
+    "url": "/mz-react-portfolio/static/media/github.f75850c7.png"
+  },
+  {
+    "revision": "4287f7bc3fe2901521e18c24ef2de928",
+    "url": "/mz-react-portfolio/static/media/mz.4287f7bc.png"
+  },
+  {
+    "revision": "de251c5dbd006d12771ed1ba648fa38c",
+    "url": "/mz-react-portfolio/static/media/mzlogo.de251c5d.png"
+  },
+  {
+    "revision": "786eee11da54827b48e07e9995ece7e6",
+    "url": "/mz-react-portfolio/static/media/z.786eee11.png"
+  }
+]);
