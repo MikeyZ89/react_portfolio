52cd47400a5bad4289cd8100487ce8471a2cb5c0
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-07-10 16:01:00 -0700

    commit

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Content from '../components/Content';
-import DownloadLink from "react-download-link";
+
 
 function AboutPage(props) {
 
